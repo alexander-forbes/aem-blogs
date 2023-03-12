8f2464f6e3938d9b909a8c2e3fa46dd470a0b817
Satya Deep Maheshwari <satyam@adobe.com>
2023-03-12 07:18:26 +0530

    Addressed CLS

--- blocks/ics/ics.js
+++ blocks/ics/ics.js
@@ -16,21 +16,20 @@ export default async function decorate(block) {
 
     const cfg = readBlockConfig(block);
     block.textContent = '';
-
+    var downloadBtn = document.createElement("button");
+    downloadBtn.classList.add('btn-ics-download');
+    const calIcon = document.createElement('img');
+    calIcon.src = "/icons/calendar.svg";
+    downloadBtn.appendChild(calIcon);
+    const btnText = document.createElement('p');
+    btnText.textContent = "Download ics";
+    downloadBtn.appendChild(btnText);
+    block.appendChild(downloadBtn);
     loadScript('/blocks/ics/ics.deps.min.js', () => {
         var cal = ics();
         cal.addEvent(cfg.subject, cfg.description, cfg.location, cfg.begin, cfg.end);
-        var downloadBtn = document.createElement("button");
-        downloadBtn.classList.add('btn-ics-download');
-        const calIcon = document.createElement('img');
-        calIcon.src = "/icons/calendar.svg";
-        downloadBtn.appendChild(calIcon);
-        const btnText = document.createElement('p');
-        btnText.textContent = "Download ics";
-        downloadBtn.appendChild(btnText);
         downloadBtn.addEventListener('click', () => {
             cal.download();
         });
-        block.appendChild(downloadBtn);
     });
 }
